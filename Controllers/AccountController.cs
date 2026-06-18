c160635d037cbe82dcc43212e5c8da2acb8149f3
ozanyazici9 <ozanyazici9@gmail.com>
2026-06-18 22:49:23 +0300

    feat(cart): transferring the shopping cart to the user

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SQLitePCL;
 
 namespace dotnet_store.Controllers;
 
@@ -13,16 +15,19 @@ public class AccountController : Controller
     private UserManager<AppUser> _userManager;
     private SignInManager<AppUser> _signInManager;
     private IEmailService _emailService;
+    private readonly DataContext _context;
 
     public AccountController(
         UserManager<AppUser> userManager,
         SignInManager<AppUser> signInManager,
-        IEmailService emailService
+        IEmailService emailService,
+        DataContext context
     )
     {
         _userManager = userManager;
         _signInManager = signInManager;
         _emailService = emailService;
+        _context = context;
     }
 
     public ActionResult Index()
@@ -91,6 +96,8 @@ public class AccountController : Controller
                     await _userManager.ResetAccessFailedCountAsync(user);
                     await _userManager.SetLockoutEndDateAsync(user, null);
 
+                    await TransferCartToUser(user);
+
                     if (!string.IsNullOrEmpty(returnUrl))
                     {
                         return Redirect(returnUrl);
@@ -120,6 +127,41 @@ public class AccountController : Controller
             }
         }
         return View(model);
+    }
+
+    private async Task TransferCartToUser(AppUser user)
+    {
+        var userCart = await _context
+            .Carts.Where(i => i.CustomerId == user.UserName)
+            .Include(i => i.CartItems)
+                .ThenInclude(i => i.Urun)
+            .FirstOrDefaultAsync();
+
+        var cookieCart = await _context
+            .Carts.Where(i => i.CustomerId == Request.Cookies["customerId"])
+            .Include(i => i.CartItems)
+                .ThenInclude(i => i.Urun)
+            .FirstOrDefaultAsync();
+
+        foreach (var item in cookieCart?.CartItems!)
+        {
+            var cartItem = userCart?.CartItems.FirstOrDefault(i => i.UrunId == item.UrunId);
+
+            if (cartItem != null)
+            {
+                cartItem.Miktar += item.Miktar;
+            }
+            else
+            {
+                userCart?.CartItems.Add(
+                    new CartItem { UrunId = item.UrunId, Miktar = item.Miktar }
+                );
+            }
+        }
+
+        _context.Carts.Remove(cookieCart);
+
+        await _context.SaveChangesAsync();
     }
 
     [Authorize]
