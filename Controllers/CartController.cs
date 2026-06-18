24bb78241aceb10c508ed67fbecf9a881b8c336d
ozanyazici9 <ozanyazici9@gmail.com>
2026-06-18 19:19:29 +0300

    feat(cart): associating the shopping cart with cookies

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace dotnet_store.Controllers;
 
-[Authorize]
 public class CartController : Controller
 {
     private readonly DataContext _context;
@@ -68,7 +67,8 @@ public class CartController : Controller
 
     private async Task<Cart> GetCart()
     {
-        var customerId = User.Identity?.Name;
+        // kulannıcı giriş yapmamışsa customerıd yi cookie den çek
+        var customerId = User.Identity?.Name ?? Request.Cookies["customerId"];
 
         var cart = await _context
             .Carts.Where(i => i.CustomerId == customerId) // sadece bu kullanıcının sepeti
@@ -78,7 +78,25 @@ public class CartController : Controller
 
         if (cart == null)
         {
-            cart = new Cart { CustomerId = customerId! };
+            // customerId nin olupda cart ın olmadığı durumlar olabilir kullanıcı yeni kayıt olmuştur ve sepeti henüz oluşturulmamıştır veya bir şekilde cookideki cart nesnesi silinmiştir. Bu durumlarada tekrar customerId oluşturmaya gerek yoktur bu yüzden kontrol yapılır.
+            customerId = User.Identity?.Name;
+
+            if (string.IsNullOrEmpty(customerId))
+            {
+                // customerıd yi cookie den de alamazsa customerıd oluşturuyoruz ve bunu cart oluşturmada kullanıyoruz
+                customerId = Guid.NewGuid().ToString();
+
+                var cookieOptions = new CookieOptions
+                {
+                    Expires = DateTime.Now.AddMonths(1),
+                    IsEssential = true,
+                };
+
+                // cookie sunucu tarafında burada oluşturuluyor ve response da client a variliyor. Bizde GetCart da cookie yi client dan gelen request üzerinden okuyarak kontrol ediyoruz
+                Response.Cookies.Append("customerId", customerId, cookieOptions);
+            }
+
+            cart = new Cart { CustomerId = customerId };
             _context.Carts.Add(cart); // eğer cart null ise kullanıcı kayıtlı değildir. burada da savechanges demiyoruz ama change tracking sistemi sayesinde context e eklemiş oluyoruz ve cart nesnesi kontrolümüz altında oluyor daha sonra cart ı geri döndürüyoruz. eğer giriş yapamamış kullanıcı sepete ürün eklerse cart a ürünüde ekleyip db ye kaydediyoruz.
         }
 
